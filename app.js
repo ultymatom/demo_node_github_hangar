66ebdf7850e5fbda2fac5c68a18722ce51d5d6cb
ultymatom <90399827+ultymatom@users.noreply.github.com>
2022-08-04 09:23:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/hangar', function (req, res) {
-  res.send("Hello World2");
+  res.send("Hello World3");
 });
 
 app.get('/ping', function (req, res) {
