c0085abc795b469d4a4366bb92ec1809e9825db4
ultymatom <90399827+ultymatom@users.noreply.github.com>
2022-08-30 13:21:03 +0200

    Update obtain-dns.sh

--- .github/workflows/scripts/obtain-dns.sh
+++ .github/workflows/scripts/obtain-dns.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-dnsName=$(kubectl get svc --namespace nginx-ingress nginx-ingress-nginx-ingress-controller -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
-while test -z "$dnsName"
+ip="$(kubectl get svc nginx-ingress-nginx-ingress-controller --namespace nginx-ingress -o jsonpath='{.status.loadBalancer.ingress[0].ip}')"
+
+while test -z "$ip"
 do
     sleep 5s
-    dnsName=$(kubectl get svc --namespace nginx-ingress nginx-ingress-nginx-ingress-controller -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
+    ip="$(kubectl get svc nginx-ingress-nginx-ingress-controller --namespace nginx-ingress -o jsonpath='{.status.loadBalancer.ingress[0].ip}')"
 done
-echo "##vso[task.setvariable variable=dns;isOutput=true]$dnsName"
+
+# Obtain the AKS cluster name 
+dnsname=$1
+
+ipname=$(az network public-ip list --query "[?ipAddress!=null]|[?contains(ipAddress, '$ip')].[name]" --output tsv)
+
+iprg=$(az network public-ip list --query "[?ipAddress!=null]|[?contains(ipAddress, '$ip')].[resourceGroup]" --output tsv)
+
+az network public-ip update --resource-group "$iprg" --name "$ipname" --dns-name "$dnsname"
+
+dns="$(az network public-ip show --resource-group "$iprg" --name "$ipname" --query "[dnsSettings.fqdn]" --output tsv)"
+
+echo "$dns"
